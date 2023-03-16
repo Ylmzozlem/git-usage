4429bb4f6052c7e0f94a6f54b9207b9985422e01
Ozlem OKUR <ylmzozlem.3461@gmail.com>
2023-03-16 21:46:32 +0300

    second

--- src/main/java/GitUsage.java
+++ src/main/java/GitUsage.java
@@ -1,6 +1,9 @@
 public class GitUsage {
     public static void main(String[] args) {
         System.out.println("This is work space");
+        if(true){
+            System.out.println("I send all changes to stage area");
+        }
     }
 
 }
