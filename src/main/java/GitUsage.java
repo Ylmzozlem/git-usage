7041d08a2afc643b545680a30e2a1a6eb665b795
Ozlem OKUR <ylmzozlem.3461@gmail.com>
2023-03-16 22:09:01 +0300

    My fault

--- src/main/java/GitUsage.java
+++ src/main/java/GitUsage.java
@@ -4,6 +4,10 @@ public class GitUsage {
         if(true){
             System.out.println("I send all changes to stage area");
         }
+
+        System.out.println("Don'd forget to commit me");
+        System.out.println("again");
+
     }
 
 }
