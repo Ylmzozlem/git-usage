986b8d19b052f45bf9a2046eff08b798f5d89dca
Ozlem OKUR <ylmzozlem.3461@gmail.com>
2023-03-31 15:19:31 +0300

    A3

--- src/main/java/Student2.java
+++ src/main/java/Student2.java
@@ -2,7 +2,7 @@ public class Student2 {
     public static void main(String[] args) {
 
         System.out.println("Ozlem will do something");
-    }
+
         System.out.println("Ozlem will do something");
         System.out.println("I m trying somethings" +
                 ",");
