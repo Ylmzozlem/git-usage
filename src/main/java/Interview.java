91f692ab571137273a72550a420cdd50a8934145
Ozlem OKUR <ylmzozlem.3461@gmail.com>
2023-03-31 12:31:04 +0300

    Ali's changes

---
+++ src/main/java/Interview.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Interview {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/package1/Student.java
@@ -0,0 +1,18 @@
+package package1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Student {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10, 4, -1, 5);
+
+        Collections.sort(list);
+
+        Integer array[] = list.toArray(new Integer[5]);
+
+        System.out.println(Arrays.toString(array));
+        System.out.println("Student changes");
+    }
+}
