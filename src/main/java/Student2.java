9f05e420fcf45393d60ff7644eece50d4d695791
Ozlem OKUR <ylmzozlem.3461@gmail.com>
2023-03-31 14:26:10 +0300

    A2

---
+++ src/main/java/Student2.java
@@ -0,0 +1,11 @@
+public class Student2 {
+    public static void main(String[] args) {
+
+        System.out.println("Ozlem will do something");
+        System.out.println("I m trying somethings" +
+                ",");
+        int a=5;
+        int b=4;
+
+    }
+}
